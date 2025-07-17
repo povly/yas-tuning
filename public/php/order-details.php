5cc38f0158e37c7c8f853f5214cd61be4f4db9e0
Anatoly <povly19995@gmail.com>
2025-07-17 18:36:35 +0300

    Refactor order details modal and enhance user experience
    - Updated order-details.php to improve modal close functionality by resetting body overflow.
    - Changed download buttons to links for better accessibility and user interaction.
    - Enhanced styling in user.scss for improved layout and visual consistency.
    - Added event listener in user.pug to manage body overflow during modal display.

--- public/php/order-details.php
+++ public/php/order-details.php
@@ -22,18 +22,18 @@ $orderData = [
 // Возвращаем HTML модального окна
 ?>
 
-<div class="modal order-detail-modal active" id="order-modal-<?php echo $orderId; ?>" onclick="document.getElementById('order-modal-<?php echo $orderId; ?>').remove()">
-  <div class="modal__table" onclick="document.getElementById('order-modal-<?php echo $orderId; ?>').remove()">
+<div class="modal order-detail-modal active" id="order-modal-<?php echo $orderId; ?>" onclick="document.getElementById('order-modal-<?php echo $orderId; ?>').remove();document.body.style.overflow='';">
+  <div class="modal__table" onclick="document.getElementById('order-modal-<?php echo $orderId; ?>').remove();document.body.style.overflow='';">
     <div class="modal__ceil">
       <div class="modal__content order-detail-modal__content" onclick="event.stopPropagation()">
-        <button class="order-detail-modal__close modal__close" onclick="document.getElementById('order-modal-<?php echo $orderId; ?>').remove()">
+        <button class="order-detail-modal__close modal__close" onclick="document.getElementById('order-modal-<?php echo $orderId; ?>').remove();document.body.style.overflow='';">
           <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2.38562 0.392716L8 6.0071L13.5853 0.421806C13.7087 0.290489 13.8573 0.185441 14.0222 0.11296C14.1872 0.0404785 14.3651 0.00205717 14.5453 0C14.931 0 15.301 0.153242 15.5737 0.426014C15.8465 0.698786 15.9998 1.06874 15.9998 1.4545C16.0032 1.63283 15.9701 1.80996 15.9025 1.97504C15.835 2.14012 15.7345 2.28966 15.607 2.41447L9.94903 7.99976L15.607 13.6578C15.8468 13.8923 15.9873 14.2099 15.9998 14.545C15.9998 14.9308 15.8465 15.3007 15.5737 15.5735C15.301 15.8463 14.931 15.9995 14.5453 15.9995C14.3599 16.0072 14.1749 15.9763 14.0022 15.9087C13.8294 15.8411 13.6726 15.7383 13.5417 15.6068L8 9.99243L2.40017 15.5923C2.27727 15.7192 2.13045 15.8205 1.96818 15.8904C1.80591 15.9603 1.63141 15.9974 1.45474 15.9995C1.06898 15.9995 0.699023 15.8463 0.42625 15.5735C0.153478 15.3007 0.000236657 14.9308 0.000236657 14.545C-0.00315451 14.3667 0.0299309 14.1896 0.0974628 14.0245C0.164995 13.8594 0.265548 13.7099 0.392952 13.5851L6.05097 7.99976L0.392952 2.34175C0.153228 2.10722 0.0126576 1.78964 0.000236657 1.4545C0.000236657 1.06874 0.153478 0.698786 0.42625 0.426014C0.699023 0.153242 1.06898 0 1.45474 0C1.80382 0.00436351 2.13836 0.14545 2.38562 0.392716Z" fill="#999999" />
           </svg>
         </button>
 
         <div class="order-detail-modal__table">
-                    <!-- Таблица заказа -->
+          <!-- Таблица заказа -->
           <div class="order-detail-modal__content-table">
             <div class="order-detail-modal__row">
               <div class="order-detail-modal__label">Start</div>
@@ -73,7 +73,7 @@ $orderData = [
             <div class="order-detail-modal__row">
               <div class="order-detail-modal__label">Priority</div>
               <div class="order-detail-modal__value">
-                <span class="order-detail-modal__priority"><?php echo $orderData['priority']; ?></span>
+                <span class="order-detail-modal__priority color_red"><?php echo $orderData['priority']; ?></span>
               </div>
             </div>
 
@@ -86,16 +86,9 @@ $orderData = [
               <div class="order-detail-modal__label">Original file</div>
               <div class="order-detail-modal__value">
                 <?php if ($orderData['status'] == 'Completed'): ?>
-                <div class="order-detail-modal__download order-detail-modal__download--active">
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 13L1 14C1 15.6569 2.34315 17 4 17L14 17C15.6569 17 17 15.6569 17 14L17 13M9 12L9 1M9 12L5 8M9 12L13 8" stroke="#F56E0F" stroke-width="2" />
-                  </svg>
-                  Download
-                </div>
+                  <a class="user__download user__download--active" href="#!">Download</a>
                 <?php else: ?>
-                <div class="order-detail-modal__download order-detail-modal__download--disabled">
-                  Download
-                </div>
+                  <button class="user__download user__download--disabled">Download</button>
                 <?php endif; ?>
               </div>
             </div>
@@ -103,12 +96,7 @@ $orderData = [
             <div class="order-detail-modal__row">
               <div class="order-detail-modal__label">Ini file</div>
               <div class="order-detail-modal__value">
-                <div class="order-detail-modal__file-output">
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 13L1 14C1 15.6569 2.34315 17 4 17L14 17C15.6569 17 17 15.6569 17 14L17 13M9 12L9 1M9 12L5 8M9 12L13 8" stroke="#F5F5F5" stroke-width="2" />
-                  </svg>
-                  File output
-                </div>
+                <a class="user__download user__download--active" href="#!">Download</a>
               </div>
             </div>
 
@@ -121,16 +109,9 @@ $orderData = [
               <div class="order-detail-modal__label">Modified file</div>
               <div class="order-detail-modal__value">
                 <?php if ($orderData['status'] == 'Completed'): ?>
-                <div class="order-detail-modal__download order-detail-modal__download--active">
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 13L1 14C1 15.6569 2.34315 17 4 17L14 17C15.6569 17 17 15.6569 17 14L17 13M9 12L9 1M9 12L5 8M9 12L13 8" stroke="#F56E0F" stroke-width="2" />
-                  </svg>
-                  Download
-                </div>
+                  <a class="user__download user__download--active" href="#!">Download</a>
                 <?php else: ?>
-                <div class="order-detail-modal__download order-detail-modal__download--disabled">
-                  Download
-                </div>
+                  <button class="user__download user__download--disabled">Download</button>
                 <?php endif; ?>
               </div>
             </div>
@@ -138,9 +119,7 @@ $orderData = [
             <div class="order-detail-modal__row">
               <div class="order-detail-modal__label">Order status</div>
               <div class="order-detail-modal__value">
-                <div class="order-detail-modal__status order-detail-modal__status--completed">
-                  <?php echo $orderData['status']; ?>
-                </div>
+                <div class="user__status user__status--completed">Сompleted</div>
               </div>
             </div>
           </div>
