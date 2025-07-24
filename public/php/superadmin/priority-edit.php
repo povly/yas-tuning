4ca8c65f2d7356810f7b2a5f37d24c1bb109e491
Anatoly <povly19995@gmail.com>
2025-07-25 01:32:42 +0300

    Add priority selection checkboxes in superadmin orders
    - Wrap priority items in labels with checkboxes for better UX
    - Update SCSS for styling consistency
    - Fix class name typo in recharge view

--- public/php/superadmin/priority-edit.php
+++ public/php/superadmin/priority-edit.php
@@ -1,4 +1,5 @@
-<div class="user__set-priority-item">
+<label class="user__set-priority-item">
+  <input class="user__set-priority-input" type="checkbox" name="priority[13]">
   <div class="user__set-priority-square"></div>
   <div class="user__set-priority-title">
     <input type="text" name="priority__title" value="2-4 hours">
@@ -6,4 +7,4 @@
   <div class="user__set-priority-price">
     <input type="text" name="priority__price" value="+20%">
   </div>
-</div>
+</label>
