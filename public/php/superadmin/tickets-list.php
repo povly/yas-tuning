98e6e57fc79e3feda7f98d479296220c59e78084
Anatoly <povly19995@gmail.com>
2025-08-05 10:38:24 +0300

    add tickets for scroll view

--- public/php/superadmin/tickets-list.php
+++ public/php/superadmin/tickets-list.php
@@ -12,3 +12,45 @@
   <div class="user__chat-tickets-text">#28012024-3</div>
   <div class="user__chat-tickets-count">2</div>
 </div>
+<div class="user__chat-tickets-item unread" hx-get="/php/superadmin/chat-ticket-view.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-6</div>
+  <div class="user__chat-tickets-count">12</div>
+</div>
+<div class="user__chat-tickets-item read" hx-get="/php/superadmin/chat-ticket-view-2.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+</div>
+<div class="user__chat-tickets-item read" hx-get="/php/superadmin/chat-ticket-view.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+</div>
+<div class="user__chat-tickets-item unread" hx-get="/php/superadmin/chat-ticket-view-2.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+  <div class="user__chat-tickets-count">2</div>
+</div>
+<div class="user__chat-tickets-item unread" hx-get="/php/superadmin/chat-ticket-view.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-6</div>
+  <div class="user__chat-tickets-count">12</div>
+</div>
+<div class="user__chat-tickets-item read" hx-get="/php/superadmin/chat-ticket-view-2.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+</div>
+<div class="user__chat-tickets-item read" hx-get="/php/superadmin/chat-ticket-view.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+</div>
+<div class="user__chat-tickets-item unread" hx-get="/php/superadmin/chat-ticket-view-2.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+  <div class="user__chat-tickets-count">2</div>
+</div>
+<div class="user__chat-tickets-item unread" hx-get="/php/superadmin/chat-ticket-view.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-6</div>
+  <div class="user__chat-tickets-count">12</div>
+</div>
+<div class="user__chat-tickets-item read" hx-get="/php/superadmin/chat-ticket-view-2.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+</div>
+<div class="user__chat-tickets-item read" hx-get="/php/superadmin/chat-ticket-view.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+</div>
+<div class="user__chat-tickets-item unread" hx-get="/php/superadmin/chat-ticket-view-2.php?ticket_id=123" hx-trigger="selectTicket" hx-target=".user__chat-right-content" hx-swap="innerHTML">
+  <div class="user__chat-tickets-text">#28012024-3</div>
+  <div class="user__chat-tickets-count">2</div>
+</div>
